9a2ece33e18557abc8a269120b95842fa0e80f38
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-06 14:52:51 +0100

    solved vowelcount

--- classes/7kyu.cs
+++ classes/7kyu.cs
@@ -1,4 +1,17 @@
 public class KyuSeven
 {
-
+    public static int GetVowelCount(string str)
+    {
+        // alternative solution return str.Count(i => "aeiou".Contains(i));
+        int vowelCount = 0;
+        string vowels = "aeiou";
+        foreach (char ch in str)
+        {
+            if (vowels.Contains(ch))
+            {
+                vowelCount++;
+            }
+        }
+        return vowelCount;
+    }
 }
