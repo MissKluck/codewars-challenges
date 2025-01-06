370c0be9660ba9da1fdc4e14d35fb89b370e8324
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-06 14:22:33 +0100

    setup 8kyu and implemented it in program

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {
         KyuSix k6 = new KyuSix();
+        KyuEight k8 = new KyuEight();
         Console.WriteLine(k6.Multiples(10));
     }
 }
---
+++ classes/8kyu.cs
@@ -0,0 +1,16 @@
+public class KyuEight
+{
+    // alternate solution; return number % 2 == 0 ? "Even" : "Odd";
+    public static string EvenOrOdd(int number)
+    {
+        if (number % 2 == 0)
+        {
+            return "Even";
+        }
+        else
+        {
+            return "Odd";
+        }
+        // Code goes here ...
+    }
+}
