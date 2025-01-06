7f3c41a013a194ff247083905947e60111dc1de1
MissKluck <sigrid.b.kluck@gmail.com>
2025-01-06 12:42:45 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace codewars_challenges;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KyuSix k6 = new KyuSix();
+        Console.WriteLine(k6.Multiples(10));
+    }
+}
---
+++ classes/kyu6.cs
@@ -0,0 +1,15 @@
+public class KyuSix
+{
+    public int Multiples(int value)
+    {
+        int result = 0;
+        for (int i = 0; i < value; i++)
+        {
+            Console.WriteLine(i);
+            i % 3 = 1;
+            i % 5 =
+            result += i;
+        }
+        return 0;
+    }
+}
